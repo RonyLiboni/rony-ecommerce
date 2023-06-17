c0d9f4531a7769286039b427c820f43919842506
ronaldLiboni <ronald.liboni@acad.pucrs.br>
2023-06-17 11:18:21 -0300

    first commit

---
+++ src/main/java/br/com/rony/ecommerce/RonyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rony.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonyEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rony/ecommerce/RonyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rony.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
