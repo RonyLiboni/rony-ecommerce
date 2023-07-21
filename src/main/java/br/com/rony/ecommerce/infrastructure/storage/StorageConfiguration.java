688c35b11ff26382e1a587b44627070a71da4c7d
ronaldLiboni <ronald.liboni@acad.pucrs.br>
2023-07-20 22:13:54 -0300

    FIX creates the folders needed for local storage

--- src/main/java/br/com/rony/ecommerce/infrastructure/storage/StorageConfiguration.java
+++ src/main/java/br/com/rony/ecommerce/infrastructure/storage/StorageConfiguration.java
@@ -1,5 +1,10 @@
 package br.com.rony.ecommerce.infrastructure.storage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,22 +15,33 @@ import br.com.rony.ecommerce.adapters.infrastructure.storage.S3ImageStorageServi
 
 @Configuration
 public class StorageConfiguration {
-	
+
 	private final StorageProperties storageProperties;
-		
+
 	public StorageConfiguration(StorageProperties storageProperties) {
 		this.storageProperties = storageProperties;
 	}
 
 	@Bean
 	ImageStorageService imageStorageService(StorageErrorRepository storageErrorRepository, AmazonS3 amazonS3) {
-		if(storageProperties.getAccessKeyId().isBlank() ||
-		   storageProperties.getSecretAccessKey().isBlank() ||
-		   storageProperties.getBucket().isBlank() ||
-		   storageProperties.getRegion() == null) {
-			return new LocalImageStorageService(storageErrorRepository);	
+		if (storageProperties.getAccessKeyId().isBlank() || storageProperties.getSecretAccessKey().isBlank()
+				|| storageProperties.getBucket().isBlank() || storageProperties.getRegion() == null) {
+			try {
+				Path caminhoDiretorios = getPhotosDirectory();
+				Files.createDirectories(caminhoDiretorios);
+			} catch (Exception e) {
+			}
+			return new LocalImageStorageService(storageErrorRepository);
 		}
-		return new S3ImageStorageService(amazonS3, storageProperties, storageErrorRepository);	
+		return new S3ImageStorageService(amazonS3, storageProperties, storageErrorRepository);
 	}
-	
+
+	private Path getPhotosDirectory() {
+		try {
+			return Path.of(new File(".").getCanonicalPath(), "/src/main/resources/product_images/");
+		} catch (IOException e) {
+			throw new StorageException("Chosen local directory is unavailable.");
+		}
+	}
+
 }
